a975fed7a69cddff786660bd7d4466b80a3e6758
abikisyo <85157766+abikisyo@users.noreply.github.com>
2021-06-21 12:55:32 +0900

    제출

--- select.sql
+++ select.sql
@@ -15,14 +15,18 @@ select * from BOOK natural join ROOM where VCODE = 'CTV' order by RPRICE desc;
 --총 예약 횟수,총 매출액
 select * from PROFIT;
 --차량(예약번호 순)
-select * from CAR order by BNUM;
+select * from CAR order by BNUM desc;
 --예약시 날짜가 21년 12월 10일 인 예약
-select * from BOOK where BDATE = '21-12-10';
+select * from BOOK where BDATE='21/12/10';
 --숙박기간이 1박 2일 인 예약
-select * from BOOK where CHECKOUT - CHECKIN =1;
+select * from BOOK where CHECKOUT-CHECKIN=1;
 --12층의 객실별 예약건수
-select  RNUM,count(BNUM)from BOOK group by RNUM having RNUM between 1200 and 1300 order by RNUM;
---checkOUT 기준 6월 일별 매출(날짜순 정렬)
-select checkout, sum(PAMOUNT) from PAYMENT natural join BOOK group by checkout having checkout between '21-06-01' and '21-06-30' order by checkout ;
---checkIN8월달의 OCV인 예약 (날짜순 정렬)
-select * from BOOK  natural join ROOM  where VCODE = 'OCV' and CHECKIN between '21-08-01'and'21-08-31' order by CHECKIN;
+select VCODE, count(BNUM) from BOOK natural join ROOM where RNUM between 1200 and 1300 group by VCODE;
+--checkout 기준 6월 일별 매출(날짜순 정렬)
+select CHECKOUT, sum(PAMOUNT) from BOOK natural join PAYMENT 
+      group by CHECKOUT 
+        having CHECKOUT between '21/06/01' and '21/6/30'
+        order by CHECKOUT desc;
+--checkIN 8월달의 OCV인 예약 (날짜순 정렬
+select * from ROOM natural join BOOK where CHECKIN between '21-08-01' and '21-08-31' and VCODE='OCV' order by checkin;
+select CHECKIN ,count(VCODE) from ROOM natural join BOOK where  VCODE='OCV' group by CHECKIN  having CHECKIN between '21-08-01' and '21-08-31' order by CHECKIN;
